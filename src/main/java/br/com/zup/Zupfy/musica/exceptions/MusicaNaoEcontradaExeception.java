0aae964cdd5d1e3fbb8f9b99f47a09fa1f49d977
Debora Souza <deboral.p@hotmail.com>
2021-12-10 11:17:23 -0300

    Adicinar contrutor para mensagem

--- src/main/java/br/com/zup/Zupfy/musica/exceptions/MusicaNaoEcontradaExeception.java
+++ src/main/java/br/com/zup/Zupfy/musica/exceptions/MusicaNaoEcontradaExeception.java
@@ -1,4 +1,7 @@
 package br.com.zup.Zupfy.musica.exceptions;
 
 public class MusicaNaoEcontradaExeception extends RuntimeException{
+    public MusicaNaoEcontradaExeception(String message) {
+        super(message);
+    }
 }
