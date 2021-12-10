117f98c0bc3f35291f16a2bffad10383ddba3eaa
Debora Souza <deboral.p@hotmail.com>
2021-12-10 14:43:47 -0300

    Modelar classe de Controller Advisor

--- src/main/java/br/com/zup/Zupfy/Configu/ControllerAdvisor.java
+++ src/main/java/br/com/zup/Zupfy/Configu/ControllerAdvisor.java
@@ -1,4 +1,39 @@
 package br.com.zup.Zupfy.Configu;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
 public class ControllerAdvisor {
-}
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<ErroDeValidacao> manipularErrosDeValidacao(MethodArgumentNotValidException exception) {
+        List<ErroDeValidacao> erros = new ArrayList<>();
+        for (FieldError fieldError : exception.getFieldErrors()) {
+            ErroDeValidacao erroDeValidacao = new ErroDeValidacao(
+                    fieldError.getDefaultMessage(),fieldError.getField());
+            erros.add(erroDeValidacao);
+        }
+        return erros;
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public MensagemDeErro manipularExcecaoEnumInvalido(HttpMessageNotReadableException exception){
+        if(exception.getLocalizedMessage().contains("br.com.zup.GerenciarContas.enums.Tipo")){
+            return new MensagemDeErro("Opção não encontrada");
+        }
+        return new MensagemDeErro(exception.getLocalizedMessage());
+    }
+
+    }
