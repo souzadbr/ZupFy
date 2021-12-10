2386f4c932fc600f5ae0892ef9f27aa51f6db23b
Debora Souza <deboral.p@hotmail.com>
2021-12-10 14:44:14 -0300

    Criar e modelar classe de ErroDeValidacao

--- src/main/java/br/com/zup/Zupfy/Configu/ErroDeValidacao.java
+++ src/main/java/br/com/zup/Zupfy/Configu/ErroDeValidacao.java
@@ -1,2 +1,28 @@
-package br.com.zup.Zupfy.Configu;public class ErroDeValidacao {
+package br.com.zup.Zupfy.Configu;
+
+public class ErroDeValidacao {
+    private String campo;
+    private String mensagem;
+
+    public ErroDeValidacao(String mensagem, String campo) {
+        this.mensagem = mensagem;
+        this.campo = campo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
 }
+
