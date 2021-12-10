b8c0fc8210e6374a3958afbed9a845308b04e54f
Debora Souza <deboral.p@hotmail.com>
2021-12-10 14:24:40 -0300

    Criar post na classe controller

--- src/main/java/br/com/zup/Zupfy/musica/MusicaController.java
+++ src/main/java/br/com/zup/Zupfy/musica/MusicaController.java
@@ -1,7 +1,28 @@
 package br.com.zup.Zupfy.musica;
 
-import org.springframework.web.bind.annotation.RestController;
+import br.com.zup.Zupfy.musica.dtos.MusicaCadastroDTO;
+import br.com.zup.Zupfy.musica.dtos.MusicaDetalhesDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/musicas")
 public class MusicaController {
+
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private MusicaService musicaService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MusicaDetalhesDTO cadastrarMusica (@RequestBody @Valid MusicaCadastroDTO musicaCadastroDTO){
+        Musica musica = modelMapper.map(musicaCadastroDTO, Musica.class);
+
+        return modelMapper.map(musicaService.cadastrarMusica(musica),MusicaDetalhesDTO.class);
+    }
 }
